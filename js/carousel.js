dcbc4dd09081a7bc1cddb926faa28eb2b74c01be
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2017-08-02 21:56:03 -0400

    Implement alpha version

---
+++ js/carousel.js
@@ -0,0 +1,99 @@
+var carousel = (function() {
+
+  var config = { };
+
+  config.slides       = 4;
+  config.itemScroll   = 1;
+  config.speed        = 500;
+  config.effect       = "ease";
+  config.delay        = 0;
+  config.transition   = 'margin-left ' + config.speed + 'ms' + ' ' + config.effect + ' ' + config.delay + 'ms';
+  config.carousel     = ".carousel";
+  config.carouselList = ".carousel .list";
+  config.nextName     = "triangleNext"; //btn-next
+  config.prevName     = "trianglePrev"; //btn-prev
+  config.btnList      = createMarkup.newElement("div", {"class" : "btn-list"});
+  config.btnNext      = createMarkup.newElement("div", {"id" : config.nextName}, null);
+  config.btnPrev      = createMarkup.newElement("div", {"id" : config.prevName}, null);
+
+
+  function addTransition(){
+    document.querySelector(config.carouselList).style.transition = config.transition;
+  }
+
+function createButtons(){
+    config.btnList.appendChild(config.btnPrev);
+    config.btnList.appendChild(config.btnNext);
+    document.querySelector(config.carousel).appendChild(config.btnList);
+    goNext();
+    goPrev();
+}
+
+  function goNext() {
+    document.getElementById(config.nextName).addEventListener("click", function(){
+      var scroll = document.querySelector(config.carouselList).style.marginLeft.replace("px",""),
+          stopCondition = -1*(config.totalWidth - (config.slides * config.itemWidth ) );
+
+      scroll = (scroll*1) - config.itemWidth * config.itemScroll;
+
+      if(scroll >= stopCondition){
+        roll(scroll);
+      } else {
+        roll(stopCondition);
+      }
+
+    });
+
+  }
+
+    function goPrev(){
+        document.getElementById(config.prevName).addEventListener("click", function(){
+        var scroll = document.querySelector(config.carouselList).style.marginLeft.replace("px","");
+        scroll = (scroll*1) + config.itemWidth * config.itemScroll;
+        if(scroll <= 0 ){
+            roll(scroll);
+            }
+        });
+    }
+
+  function roll(scroll){
+    document.querySelector(config.carouselList).style.marginLeft = scroll + "px";
+  }
+
+
+  function init(){
+    // Listen for the event.
+    document.addEventListener('completeRecommendation', function (e) {
+       setItemWidth();
+       addTransition();
+       createButtons();
+     }, false);
+
+  }
+
+  function setItemWidth(){
+    var list    = document.querySelector(".carousel .list"),
+        item    = list.getElementsByClassName("item"),
+        listW   = list.offsetWidth,
+        itemW   = listW / config.slides,
+        totalW  = itemW * item.length ; // + 5
+
+    config.listWidth = listW;
+    config.itemWidth = itemW;
+    config.totalWidth = totalW;
+
+    for(var i=0; i < item.length; i++) {
+      list.childNodes[i].style.width = itemW + "px";
+    }
+    list.style.width = totalW+"px";
+    console.log("totalW");
+    console.log(totalW);
+
+  }
+  return {
+    init: init
+  };
+
+})();
+
+carousel.init();
---
+++ js/vitrine.js
@@ -0,0 +1,115 @@
+var createMarkup = (function() {
+
+function getData(data)
+{
+    var el = document.getElementById("chaordicOnSite"); // vascon; carousel
+    // console.log('chaordicOnSite')
+    createReference(data);
+    createRecommendation(data);
+}
+
+function createItem(obj, selector){
+    var reference = selector;
+    var item      = obj;
+    var container = newElement("li", {"class" :"item" });
+    var figure    = newElement("figure", {"class" : "figure-align"});
+    var a         = newElement("a", {'class': "linkImg" ,"href" : "http:" + item.detailUrl });
+    var img       = newElement("img", {"src" : "http:" + item.imageName });
+    var title     = newElement("span", {"class" : "text"}, item.name );
+    var oldPrice  = item.oldPrice  ? newElement("span", {"class" : "old-price"}, "De: " + item.oldPrice) : false;
+    var price     = newElement("p", {"class" : "price"}, "Por:<span class=\"bigger\"> " + item.price + '</span>');
+    var paymentConditions   = newElement("p", {"class" : "paymentConditions"}, item.productInfo.paymentConditions);
+    var interest   = newElement("p", {"class" : "interest"}, "Sem juros");
+    // console.log("item.detailUrl");
+    // console.log(item.detailUrl);
+
+
+    a.appendChild(img);
+    figure.appendChild(a);
+
+    container.appendChild(figure);
+
+    container.appendChild(title);
+    if(oldPrice){
+        container.appendChild(oldPrice);
+    }
+    container.appendChild(price);
+    container.appendChild(paymentConditions);
+    container.appendChild(interest);
+    reference.appendChild(container);
+}
+
+function createReference(obj){
+    var list = newElement("ul", {"class" : "list"}),
+        selector = document.querySelector(".reference");
+
+    selector.appendChild(list);
+    createItem(obj.reference.item, selector.querySelector(".list") );
+}
+
+  function createRecommendation(obj){
+    var recommend = obj.recommendation,
+        list = newElement("ul", {"class" : "list"}),
+        selector = document.querySelector(".recommendation .carousel"),
+        event = new Event("completeRecommendation");
+
+    selector.appendChild(list);
+    var i = 1;
+    for(var prop in recommend) {
+      createItem(recommend[prop], document.querySelector(".recommendation .list")  );
+      //showData(recommend[prop]);
+
+      if(recommend.length === i++){
+        // Dispatch the event.
+        document.dispatchEvent(event);
+      }
+    }
+
+  }
+
+  HTMLElement.prototype.text = function(text) {
+    this.innerHTML = text;
+  }
+
+  function showData(obj){
+    for (var prop in obj) {
+    }
+  }
+
+  function newElement(element, attr, text) {
+    var el = document.createElement(element);
+    for (var prop in attr) {
+      el.setAttribute(prop, attr[prop]);
+    }
+
+    if(text !== undefined){
+      el.text(text);
+    }
+    return el;
+  }
+
+function documentReady(){
+    var script = document.createElement('script');
+    script.src = "http://roberval.chaordicsystems.com/challenge/challenge.json?callback=X";
+    script.src = "js/local_data.json?callback=X";
+    document.getElementsByTagName('body')[0].appendChild(script);
+}
+
+
+    function init() {
+        documentReady();
+    }
+
+  return {
+    getData : getData,
+    init : init,
+    newElement: newElement
+  };
+
+})();
+
+createMarkup.init();
+
+function X(data){
+  createMarkup.getData(data.data);
+}
